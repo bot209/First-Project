7b77d7f8015cb30b23893454d218e37dbc15282f
bot209 <bot2014cs@gmail.com>
2022-06-30 18:30:14 +0300

    update/first_project

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,7 @@
-from selenium.webdriver.common.by import By
-import time
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def test_link_basket_button(browser):
-    browser.get(link)
-    time.sleep(30)
-    button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-    assert button, 'Button is not found'
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser,link)       # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                         # открываем страницу
+    page.go_to_login_page()             # выполняем метод страницы — переходим на страницу логина
