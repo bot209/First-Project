55c40c7fb5316fd068d3f828ab3d4fb1745e8c27
bot209 <bot2014cs@gmail.com>
2022-07-03 21:17:45 +0300

    update/basket_page

--- test_main_page.py
+++ test_main_page.py
@@ -35,19 +35,21 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.in_basket_are_no_item()
     page.expect_text_that_the_basket_is_empty()
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)                              # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                                                 # открываем страницу
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)        # Переход между страницами 
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)                              # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                                                 # открываем страницу
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)        # Переход между страницами 
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
     
 def test_guest_should_be_login_url(browser):
     link = "http://selenium1py.pythonanywhere.com/"
